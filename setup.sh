9f760f6d9ee2cf66f782548008a9da4229bf1520
Alex Stefan <stefan.alex170299@gmail.com>
2024-12-15 22:42:10 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 PROJECT_DIR=$(pwd)
 
-yes | wget -O /tmp/netdata-kickstart.sh https://get.netdata.cloud/kickstart.sh && sh /tmp/netdata-kickstart.sh --stable-channel --disable-telemetry
+wget -O /tmp/netdata-kickstart.sh https://get.netdata.cloud/kickstart.sh
+chmod +x /tmp/netdata-kickstart.sh
+yes | sh /tmp/netdata-kickstart.sh --stable-channel --disable-telemetry
+
 cp $PROJECT_DIR/netdata.conf /etc/netdata/netdata.conf
 cp $PROJECT_DIR/custom_alerts.conf /etc/netdata/health.d/custom_alerts.conf
 
